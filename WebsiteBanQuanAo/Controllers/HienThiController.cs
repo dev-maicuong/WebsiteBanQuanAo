28a275564ff475f3d1ce3eb8e552648383f75921
hoangmaicuong99 <65660716+hoangmaicuong99@users.noreply.github.com>
2020-05-22 15:00:04 +0700

    rsfsd

---
+++ WebsiteBanQuanAo/Controllers/HienThiController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanQuanAo.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace WebsiteBanQuanAo.Controllers
+{
+    public class HienThiController : Controller
+    {
+        // GET: HienThi
+        DatabaseBanQuanAoEntities dt = new DatabaseBanQuanAoEntities();
+        public PartialViewResult Menu()
+        {
+            return PartialView(dt.tbDanhMucs.ToList());
+        }
+        public PartialViewResult SanPhamMoiNhat()
+        {
+            return PartialView(dt.tbSanPhams.ToList());
+        }
+        public PartialViewResult TatCaSanPham(int ? page)
+        {
+            int size = 6;
+            int number = (page ?? 1);
+            return PartialView(dt.tbSanPhams.OrderByDescending(n=>n.NgayTao).ToPagedList(number,size));
+        }
+    }
+}
--- WebsiteBanQuanAo/Controllers/HomeController.cs
+++ WebsiteBanQuanAo/Controllers/HomeController.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteBanQuanAo.Models;
+using PagedList;
+using PagedList.Mvc;
 
 namespace WebsiteBanQuanAo.Controllers
 {
     public class HomeController : Controller
     {
+        DatabaseBanQuanAoEntities dt = new DatabaseBanQuanAoEntities();
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult register()
+        {
+            return View();
+        }
+        public ActionResult checkout()
+        {
+            return View();
+        }
+        public ActionResult TatCaSanPham(int ? page)
+        {
+            int size = 6;
+            int number = (page ?? 1);
+            return View(dt.tbSanPhams.OrderByDescending(n=>n.NgayTao).ToPagedList(number,size));
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
